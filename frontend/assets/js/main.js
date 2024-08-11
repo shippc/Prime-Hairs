43a4070ec3b9bcd5a4c3dbc4df9422917d8deda4
Wilhan Mac'Arthur <wilhanmax@hotmail.com>
2024-08-10 21:59:55 -0300

    Login Form

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -29,9 +29,16 @@ searchClose.addEventListener('click', () => {
 })
 
 /* LOGIN */
-
+const login = document.getElementById('login'),
+    loginBtn = document.getElementById('login-btn'),
+    loginClose = document.getElementById('login-close')
 
 /* Login show */
-
+loginBtn.addEventListener('click', () => {
+    login.classList.add('show-login')
+})
 
 /* Login hidden */
+loginClose.addEventListener('click', () => {
+    login.classList.remove('show-login')
+})
