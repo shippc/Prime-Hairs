f58f13ecffc3fa26329e7a973211fdb927b4dd3d
Wilhan Mac'Arthur <wilhanmax@hotmail.com>
2024-08-18 12:11:44 -0300

    Contact and Footer

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -55,3 +55,33 @@ var swiper = new Swiper(".services-container", {
         rotate: 0,
     },
 });
+
+/* Email JS */
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+   e.preventDefault()
+   
+   // serviceID - templateID - #form - publicKey
+   emailjs.sendForm('service_jovs1ae','template_ngtimpg','#contact-form','dGNXLaxyY9AODOLkj')
+      .then(() => {
+         // Show sent message
+         contactMessage.textContent = 'Message sent successfully '
+
+         // Remove message after five seconds
+         setTimeout(() => {
+            contactMessage.textContent = ''
+         }, 5000)
+
+         // Clear input fields
+         contactForm.reset()
+
+      }, () => {
+         // Show error message
+         contactMessage.textContent = 'Message not sent (service error) '
+      })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
