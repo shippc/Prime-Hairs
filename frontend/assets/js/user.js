a977812586aab669d4ab83c1342c381806962f4c
shippc <wilhanmax@hotmail.com>
2024-11-06 21:20:24 -0300

    New Pages

--- frontend/assets/js/user.js
+++ frontend/assets/js/user.js
@@ -1,20 +1,15 @@
 /*===== SHOW NAVBAR  =====*/ 
 const showNavbar = (toggleId, navId, bodyId, headerId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
-    bodypd = document.getElementById(bodyId),
-    headerpd = document.getElementById(headerId);
+        nav = document.getElementById(navId),
+        bodypd = document.getElementById(bodyId),
+        headerpd = document.getElementById(headerId);
 
-    // Validate that all variables exist
     if (toggle && nav && bodypd && headerpd) {
         toggle.addEventListener('click', () => {
-            // show navbar
             nav.classList.toggle('show');
-            // change icon
             toggle.classList.toggle('bx-x');
-            // add padding to body
             bodypd.classList.toggle('body-user');
-            // add padding to header
             headerpd.classList.toggle('body-user');
         });
     }
@@ -22,8 +17,7 @@ const showNavbar = (toggleId, navId, bodyId, headerId) => {
 
 showNavbar('header-toggle-user', 'nav-bar', 'body-user', 'header-user');
 
-
-/*===== LINK ACTIVE  =====*/ 
+/*===== LINK ACTIVE  =====*/
 const linkColor = document.querySelectorAll('.nav-link-user');
 
 function colorLink() {
@@ -38,12 +32,10 @@ const sections = document.querySelectorAll('.content');
 
 // Function to show a specific section and hide the others
 function showSection(sectionId) {
-    // Hide all sections
     sections.forEach(section => {
         section.classList.remove('active');
     });
 
-    // Show the selected section
     const newContent = document.getElementById(sectionId);
     if (newContent) {
         newContent.classList.add('active');
@@ -52,27 +44,27 @@ function showSection(sectionId) {
     }
 }
 
-// Initialize by showing the Dashboard section by default
+// Show Dashboard by default on load
 window.addEventListener('load', () => {
     showSection('dashboard');
 });
 
-// Attach event listeners to sidebar links
+// Event listeners for links
 const links = document.querySelectorAll('.nav-link-user');
 links.forEach(link => {
     link.addEventListener('click', (e) => {
-        e.preventDefault();
         const sectionId = link.getAttribute('data-section');
-        showSection(sectionId);
+        if (sectionId) { // Only prevent default if data-section exists
+            e.preventDefault();
+            showSection(sectionId);
+        }
     });
 });
 
-// JavaScript para remover o preloader após a página carregar
+// Preloader
 window.addEventListener('load', function() {
     const preloader = document.getElementById('preloader');
     preloader.style.display = 'none';
-
-    // Exibir o conteúdo principal após o preloader ser removido
     const content = document.querySelector('.content');
     content.style.display = 'block';
 });
